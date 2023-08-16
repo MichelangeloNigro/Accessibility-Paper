183f97ce9b575965c4b7fc171adb308c64a1459c
mikenigro <mikenigro99@gmail.com>
2023-08-16 21:15:31 +0200

    Added text resize, change of font and save of size

--- Assets/Text Manipulation/Script/TextFinder.cs
+++ Assets/Text Manipulation/Script/TextFinder.cs
@@ -1,18 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
 
-public class TextFinder : MonoBehaviour
-{
+public class TextFinder : SingletonDDOL<TextFinder> {
+    public List<TMP_Text> texts;
+    public TMP_FontAsset dislexycAsste;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Start() {
+        texts= FindObjectsOfType<TMP_Text>(true).ToList();
+        foreach (var text in texts) {
+            text.font = dislexycAsste;
+        }
     }
 }
--- Assets/Text Manipulation/Script/TextManager.cs
+++ Assets/Text Manipulation/Script/TextManager.cs
@@ -1,18 +1,82 @@
-using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using TMPro;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class TextManager : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class TextManager : SingletonDDOL<TextManager> {
+    public wrapperTXT txts;
+
+    private void OnApplicationQuit() {
+        UpdateList();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Start() {
+        SceneManager.activeSceneChanged += AddOnChange;
+        LoadSize();
     }
+
+    private void AddOnChange(Scene current, Scene next) {
+        UpdateTextsList();
+    }
+    
+    private void UpdateTextsList() {
+        foreach (var text in FindObjectsOfType<TMP_Text>()) {
+            var existingTxt = txts.wrap.Find(txt => txt.name == text.name);
+        
+            if (existingTxt != null) {
+                existingTxt.size = text.fontSize;
+            } else {
+                var t = new txtsSize {
+                    size = text.fontSize,
+                    name = text.name
+                };
+                txts.wrap.Add(t);
+            }
+        }
+        UpdateList();
+    }
+
+
+    private void LoadSize() {
+        if (File.Exists(Application.dataPath + "/textSize.json")) {
+            string json = System.IO.File.ReadAllText(Application.dataPath + "/textSize.json");
+            var tmp = JsonUtility.FromJson<wrapperTXT>(json);
+            Debug.Log(tmp.wrap.Count);
+            foreach (var txt in tmp.wrap) {
+                foreach (var tmpText in FindObjectsOfType<TMP_Text>()) {
+                    if (tmpText.name == txt.name) {
+                        tmpText.fontSize = txt.size;
+                    }
+                }
+            }
+        }
+    }
+
+    private void UpdateList() {
+        string json = JsonUtility.ToJson(txts);
+        File.WriteAllText(Application.dataPath + "/textSize.json", json);
+    }
+
+    public void ChangeSize(float val) {
+        float tmp = math.remap(0, 1, 0, 50, val);
+        foreach (var text in TextFinder.Instance.texts) {
+            text.fontSize = tmp;
+        }
+        UpdateTextsList();
+    }
+}
+
+[System.Serializable]
+public class txtsSize {
+    public float size;
+    public string name;
+}
+
+[System.Serializable]
+public class wrapperTXT {
+    public List<txtsSize> wrap = new List<txtsSize>();
 }
